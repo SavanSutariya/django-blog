7826d26fa906c54f594e45bb02f0dabce8b9ac22
Savan Sutariya <savanpatel5248@gmail.com>
2021-11-15 22:35:50 +0530

    created base and blogs list layout

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Blog Home")
+    posts = [
+        {
+            'title' : 'Post 1',
+            'publisher' : 'Savan Sutariya',
+            'content' : 'The content of my first blog post',
+            'published_date' : 'August 28, 2021'
+        },
+        {
+            'title' : 'Post 2',
+            'publisher' : 'Mayur Patel',
+            'content' : 'The content of my Second blog post',
+            'published_date' : 'August 28, 2021'
+        },
+        {
+            'title' : 'Post 3',
+            'publisher' : 'Kishan Modasia',
+            'content' : 'The content of my Third blog post',
+            'published_date' : 'August 29, 2021'
+        }
+    ]
+    context = {'posts':posts}
+    return render(request,'blog/home.html',context)
 def about(request):
-    return HttpResponse("Blog About")
+    return render(request,'blog/about.html',{'title': 'About'})
--- django_project/urls.py
+++ django_project/urls.py
@@ -21,5 +21,5 @@ import blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
