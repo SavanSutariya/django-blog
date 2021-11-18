016cd45925d9200550a106f5de58239b20aa2783
Savan Sutariya <savanpatel5248@gmail.com>
2021-11-18 10:47:16 +0530

    created users app and register view

--- blog/views.py
+++ blog/views.py
@@ -2,26 +2,6 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 def home(request):
-    posts = [
-        {
-            'title' : 'Post 1',
-            'publisher' : 'Savan Sutariya',
-            'content' : 'The content of my first blog post',
-            'published_date' : 'August 28, 2021'
-        },
-        {
-            'title' : 'Post 2',
-            'publisher' : 'Mayur Patel',
-            'content' : 'The content of my Second blog post',
-            'published_date' : 'August 28, 2021'
-        },
-        {
-            'title' : 'Post 3',
-            'publisher' : 'Kishan Modasia',
-            'content' : 'The content of my Third blog post',
-            'published_date' : 'August 29, 2021'
-        }
-    ]
     context = {'posts':Post.objects.all()}
     return render(request,'blog/home.html',context)
 def about(request):
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from users import views as user_views
 
 import blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name="register"),
     path('', include('blog.urls')),
 ]
