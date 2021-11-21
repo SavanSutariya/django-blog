61ede862c1d185b240adc5ab1204ed8f1f9cc2ef
Savan Sutariya <savanpatel5248@gmail.com>
2021-11-21 11:29:48 +0530

    login view

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from users import views as user_views
-
+from django.contrib.auth import views as auth_views
 import blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('', include('blog.urls')),
 ]
