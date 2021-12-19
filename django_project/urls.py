a3d6db91abaf0add430d658201ef695bfefb4505
Savan Sutariya <savanpatel5248@gmail.com>
2021-12-19 11:48:56 +0530

    created User profile and signal to create user profile automatically

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.urls.conf import include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 import blog
 
 urlpatterns = [
@@ -28,3 +30,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('', include('blog.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
