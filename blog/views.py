982e2fb488e367071b90951b734f8b1ea2ab54c6
Savan Sutariya <savanpatel5248@gmail.com>
2021-12-30 12:28:50 +0530

    class based views and CRUD in posts and little UI changes

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.models import User
 # Create your views here.
-def home(request):
-    context = {'posts':Post.objects.all()}
-    return render(request,'blog/home.html',context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewType>.html
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+         
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' #<app>/<model>_<viewType>.html
+    context_object_name = "posts"
+    paginate_by = 5
+    
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 def about(request):
     return render(request,'blog/about.html',{'title': 'About'})
